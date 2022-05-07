0f85da6ec152288960e812a953712c985150a422
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-05-07 15:50:01 +0300

    add LogOut modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useState } from "react";
+import "./Modal.css";
+import classNames from "classnames";
+import { Theme, useThemeContext } from "../../context/themeModeContext";
+
+type ModalProps = {
+  active: boolean;
+  setActive: (acitve: boolean) => void;
+};
+
+const Modal: FC<ModalProps> = ({ active, setActive }) => {
+  const { theme } = useThemeContext();
+  const isLightTheme = theme === Theme.Light;
+
+  const onLogOutBtnClick = () => {
+    localStorage.setItem("isLoggedIn", "");
+    window.location.replace("/auth");
+    setActive(false);
+  };
+  return (
+    <div
+      className={active ? "modal active" : "modal"}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={classNames("modalContent", {
+          ["modalContentDark"]: !isLightTheme,
+        })}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <p className="modalText">Are you sure you want to log out?</p>
+        <div className="modalActions">
+          <button className="logoutSubmitBtn" onClick={onLogOutBtnClick}>
+            OK
+          </button>
+          <button className="logoutCancelBtn" onClick={() => setActive(false)}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/PageHeader/Sidebar/Sidebar.tsx
+++ src/components/PageHeader/Sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 import { FiMenu } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
+import Modal from "../../Modal";
 
 import { Theme, useThemeContext } from "../../../context/themeModeContext";
 import classNames from "classnames";
@@ -11,6 +12,9 @@ import classNames from "classnames";
 export default (props: any) => {
   const { theme, onChangeTheme = () => {} } = useThemeContext();
   const isLightTheme = theme === Theme.Light;
+
+  const [modalActive, setModalActive] = useState(false);
+
   return (
     <Menu
       customBurgerIcon={<FiMenu />}
@@ -37,9 +41,10 @@ export default (props: any) => {
       <a className="menu-item" href="#">
         Add posts
       </a>
-      <a className="menu-item" href="#">
+      <a className="menu-item" href="#" onClick={() => setModalActive(true)}>
         Log out
       </a>
+      <Modal active={modalActive} setActive={setModalActive} />
     </Menu>
   );
 };
