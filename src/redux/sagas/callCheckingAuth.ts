0e18cf3911d6ae581a14b6975914e037b9159158
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-06-12 16:28:59 +0300

    add EmptyState & CallCheckingAuth function

---
+++ src/redux/sagas/callCheckingAuth.ts
@@ -0,0 +1,33 @@
+//@ts-nocheck
+import { call } from "redux-saga/effects";
+import { verifyToken, getNewAccessToken } from "../api/index";
+import { setLogOut } from "../reducers/authReducer";
+
+export function* callCheckingAuth(api, ...rest) {
+  const accessToken = localStorage.getItem("jwtAccessToken");
+  const response = yield call(api, accessToken, ...rest);
+  const code = response.status;
+
+  if (code === 401) {
+    const refreshToken = localStorage.getItem("jwtRefreshToken");
+    const { status: accessStatus } = yield call(verifyToken, accessToken);
+    const { status: refreshStatus } = yield call(verifyToken, refreshToken);
+
+    if (refreshStatus === 401) {
+      yield put(setLogOut(""));
+    } else if (accessStatus === 401) {
+      const { status, data } = yield call(getNewAccessToken, refreshToken);
+      if (status === 200 && data.access && data.access.length > 0) {
+        localStorage.setItem("jwtAccessToken", data.access);
+        const refreshedResponse = yield call(api, data.access, ...rest);
+        return refreshedResponse;
+      } else {
+        yield put(setLogOut(""));
+      }
+    } else {
+      return response;
+    }
+  } else {
+    return response;
+  }
+}
